9bde809a72e6589933fdf280fd589abfcacdf234
Andre Silva <and.silva416@gmail.com>
2021-02-04 17:32:24 +0000

    iteration 2

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+let createRecipe = Recipe.create({title:"Asian Glazed Chicken Thighs"})
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -19,8 +21,12 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  createRecipe.then(() => {
+    console.log("data ok")
+    //Recipe.create({title:"",level:"Easy Peasy"})
+  })
+  .then((result) => {
+    console.log(result.title)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
